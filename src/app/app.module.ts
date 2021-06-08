1df834d262984b272e0740eb2bd70cd695acfcf1
superpuper777 <superfevrik@gmail.com>
2021-06-08 14:26:10 +0300

    fix: try fix compiler errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+
 import { SharedModule } from './components/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { VideoComponent } from './components/videos/video/video.component';
-import { VideoListComponent } from './components/videos/video-list/video-list.component';
+import { VideoListComponent } from './components/videos/video-list/video-list.component'
 import { FavoritevideosComponent } from './components/favorite-videos/favorite-videos.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
